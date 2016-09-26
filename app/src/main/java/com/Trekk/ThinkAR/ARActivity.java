2c26e4a184781f346b519bc781e6ce0166c2dc48
Tim Grable <tgrable@trekk.com>
2016-09-26 10:22:36 -0500

    Layout changes and added app icon

--- app/src/main/java/com/Trekk/ThinkAR/ARActivity.java
+++ app/src/main/java/com/Trekk/ThinkAR/ARActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
