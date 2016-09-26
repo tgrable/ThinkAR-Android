300ffdb0ff36a99a63470f5fc0981ee9aebb5895
Tim Grable <tgrable@trekk.com>
2016-09-26 16:38:38 -0500

    Added Fabric/Crahslytics

--- app/src/main/java/com/Trekk/ThinkAR/SplashActivity.java
+++ app/src/main/java/com/Trekk/ThinkAR/SplashActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,6 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
 //        Fabric.with(this, new Answers(), new Crashlytics());
         setContentView(R.layout.activity_splash);
 
